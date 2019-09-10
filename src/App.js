3167699de6306bebf445af1f357a4293e0203878
Sam <sam@sizzlecreative.co.uk>
2019-09-10 20:17:53 +0100

    Refectored the main App component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import React, {Component} from 'react';
+import React, {useState, Component} from 'react';
 import { TodoList } from './components/todo-list/TodoList';
 
 import './App.scss';
 
-export default class App extends Component{
-  constructor(props) {
-    super(props);
+export const App = () => {
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todos')));
 
-    this.state = {
-      items: JSON.parse(localStorage.getItem('todos'))
-    }
-  }
+  const _didUpdate = (value) => setItems(value);
 
-  render() {
-    return (
-      <div className="app">
-        <TodoList
-          items={this.state.items}
-          didUpdate={this._didUpdate}
-        />
-        <div className="app-author">
-          <p>A small web app by <a href="https://samogorman.dev/">Samantha O'Gorman</a></p>
-          <p>NOTE: This app only stores data to your browsers local storage.</p>
-        </div>
+  return (
+    <div className="app">
+      <TodoList
+        items={items}
+        didUpdate={_didUpdate}
+      />
+      <div className="app-author">
+        <p>A small web app by <a href="https://samogorman.dev/">Samantha O'Gorman</a></p>
+        <p>NOTE: This app only stores data to your browsers local storage.</p>
       </div>
-    );
-  }
-
-  _didUpdate = (value) => {
-    this.setState({items: value})
-  }
-
-
+    </div>
+  );
 }
